cd55b7ea7a4fc312d39ab495dc972a9e55012ce3
HeavenTao <542248377@qq.com>
2026-08-09 19:24:34 +0800

    1.update

--- src/main3.c
+++ src/main3.c
@@ -1,24 +1,12 @@
-#include <stdarg.h>
+#include <ctype.h>
 #include <stdio.h>
 
-#define HELLO 100
-
-static int myf(int count, ...) {
-
-  va_list ap;
-  va_start(ap, count);
-  int total = 0;
-  for (int j = 1; j <= count; j++) {
-    total += va_arg(ap, int);
-  }
-  va_end(ap);
-
-  return total;
-}
-
 int main() {
-  int x = myf(4, 6, 7, 8, 9);
-  printf("%d", x);
+  char s = 'h';
 
-  printf("%d", HELLO);
+  if (isupper(s)) {
+    printf("Yes issupper");
+  } else {
+    printf("No not supper");
+  }
 }
