b3ee75a40b717caa608a17f0ef2047d85f59ba02
HeavenTao <542248377@qq.com>
2026-08-18 22:29:18 +0800

    1.update

--- src/tfetextview.c
+++ src/tfetextview.c
@@ -7,11 +7,23 @@ struct _TfeTextView {
 
 G_DEFINE_FINAL_TYPE(TfeTextView, tfe_text_view, GTK_TYPE_TEXT_VIEW);
 
+static void tfe_text_view_dispose(GObject *object) {
+  TfeTextView *tv = TFE_TEXT_VIEW(object);
+
+  g_clear_object(&tv->file);
+
+  G_OBJECT_CLASS(tfe_text_view_parent_class)->dispose(object);
+}
+
 /* 实例初始化函数：创建实例时把 file 置为 NULL，表示尚未关联文件 */
 static void tfe_text_view_init(TfeTextView *tv) { tv->file = NULL; }
 
 /* 类初始化函数：目前不需要初始化类的额外内容，所以为空 */
-static void tfe_text_view_class_init(TfeTextViewClass *class) {}
+static void tfe_text_view_class_init(TfeTextViewClass *class) {
+  GObjectClass *object_class = G_OBJECT_CLASS(class);
+
+  object_class->dispose = tfe_text_view_dispose;
+}
 
 void tfe_text_view_set_file(TfeTextView *tv, GFile *file) {
   if (tv->file) {
